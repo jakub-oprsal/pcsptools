8ed6d62d858292824024e0293f798020e73e387a
Jakub Opršal <oprsal.jakub@gmail.com>
2020-12-03 17:34:37 +0000

    A simple reduction from csp to sat & structure class.

--- structure.py
+++ structure.py
@@ -1,40 +1,51 @@
 import itertools
-from collections import namedtuple
-
-Structure = namedtuple("Structure", ["domain","relations"])
-
-## HELPERS
+from functools import *
 
 def transpose(matrix):
     return tuple(zip(*matrix))
 
-def power_relation(relation, exponent):
-    yield from map(transpose, itertools.product(relation, repeat=exponent))
+def arity_of(relation):
+    rel = iter(relation)
+    try:
+        arity = len(next(rel))
+    except StopIteration:
+        return None
+
+    if not all(arity == len(edge) for edge in rel):
+        raise IndexError
+    return arity
+
+def domain_of(relation):
+    domain = set()
+    for edge in relation:
+        domain = domain.union(edge)
+    return frozenset(domain)
+
+class Structure:
+    def __init__(self, domain, relations):
+        self.domain = frozenset(domain)
+        self.relations = tuple(frozenset(relation) for relation in relations)
+
+    @cached_property
+    def type(self):
+        return tuple(map(arity_of, self.relations))
+
+    def ___or___(self, other):
+        return product_structure(self, other)
 
 def product_relation(*args, repeat=1):
     yield from map(transpose, itertools.product(*args, repeat=repeat))
 
-## FUNCTIONS
-
-def new(*args):
-    return Structure(*args)
-
-def power(structure, n):
-    power_domain = itertools.product(structure.domain, repeat=n)
-    power_rels = tuple(
-        map(lambda rel: power_relation(rel,n), structure.relations))
-
-    return Structure(power_domain,power_rels)
-
-def product(*args):
-    prod_domain = product(*[struc.domain for struc in args])
-    prod_rels = tuple( map(transpose, product(*arg_rels))
-        for arg_rels in zip(*[struc.relations for struc in args]) )
+def product_structure(*args):
+    prod_domain = itertools.product(*(struc.domain for struc in args))
+    prod_rels = tuple( product_relation(*arg_rels)
+        for arg_rels in zip(*(struc.relations for struc in args)) )
 
     return Structure(prod_domain, prod_rels)
 
-def cache(structure):
-    domain = set(structure.domain)
-    relations = tuple(set(rel) for rel in structure.relations)
+def power_structure(structure, k):
+    power_domain = itertools.product(structure.domain, repeat=k)
+    power_rels = tuple(
+        map(lambda rel: product_relation(rel, repeat=k), structure.relations) )
 
-    return Structure(domain, relations)
+    return Structure(power_domain, power_rels)
--- test.py
+++ test.py
@@ -1,16 +1,36 @@
-import structure
+from structure import *
+from reductions import *
+import pycosat
 
 def neq(n):
     yield from ( (i,j) for i in range(n) for j in range(n) if i != j )
 
-clique3 = structure.new(range(3), (neq(3),))
-print(clique3)
+def fac(n):
+    if n <= 0:
+        return 1
+    else:
+        return fac(n-1)*n
 
-clique3 = structure.cache(structure.new(range(3),(neq(3),)))
-print(clique3)
+assert(domain_of(neq(4)) == frozenset({0,1,2,3}))
 
-square = structure.power(clique3,2)
-print(square)
-for edge in square.relations[0]:
-    print(edge)
+n = 4
+clique = Structure(range(n),(neq(n),))
+assert(len(clique.relations[0]) == n*(n-1))
 
+k = 3
+powr = power_structure(clique,k)
+assert(len(powr.relations[0]) == (n*(n-1))**k)
+
+assert(powr.domain == domain_of(powr.relations[0]))
+assert(powr.type == (2,))
+
+bigger_clique = Structure(range(n+1),(neq(n+1),))
+
+sat_instance, decode = csp_to_sat((bigger_clique, clique))
+solution = pycosat.solve(sat_instance)
+assert(solution == "UNSAT")
+
+sat_instance, decode = csp_to_sat((powr, clique))
+solutions = tuple(map(decode,pycosat.itersolve(sat_instance)))
+
+assert(len(solutions) == k*fac(n))

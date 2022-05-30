bf317e3c90194fa3c9459c2d5fad3486cb1317e5
Jakub Opršal <git@jakub-oprsal.info>
2022-05-30 16:52:13 +0200

    Changing the build system.

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-
-setup(
-    name='pcsptools',
-    version='0.0.5-alpha-3',
-    description='Tools for checking identities in polymorphism minions.',
-    author='Jakub Opršal',
-    author_email='oprsal.jakub@gmail.com',
-    license='MIT',
-    packages=['pcsptools'],
-    install_requires=['pycosat']
-)
+setup()
